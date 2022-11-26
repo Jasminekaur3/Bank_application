deda0017e53a43711c9e339ffc4f7b9e64dd98e1
jasmine <66156115+Jasminekaur3@users.noreply.github.com>
2022-11-26 18:06:37 +0530

    code added

---
+++ Bank_Application.py
@@ -0,0 +1,47 @@
+import sys
+
+class Bank:
+	total_customers = 0
+
+	def __init__(self, acc_balance, name):
+		self.acc_balance = acc_balance
+		self.name = name
+		Bank.total_customers += 1
+
+	def process_customer_request(self):
+		self.greet()
+		self.show_options()
+		self.process_choices()
+
+	def greet(self):
+		print("Welcome!!")
+
+	def show_options(self):
+		print('''1.Check the balance\n 2.Deposit\n 3.Withdraw \n 4.Exit''')
+
+	def process_choices(self):
+		user_choice = int(input("Enter choices:"))
+		if user_choice == 1:
+			print(f"Your account baance is: {self.acc_balance}")
+
+		elif user_choice == 2:
+			deposit_amt = int(input("Enter amt to be deposited"))
+			self.acc_balance += deposit_amt
+			print(f"Your account baance is: {self.acc_balance}")
+
+		elif user_choice == 3:
+			withdraw_amt = int(input("Enter the amt to withdraw"))
+			self.acc_balance -= withdraw_amt
+			print(f"Your account baance is: {self.acc_balance}")
+
+		elif user_choice == 4:
+				print("Thank you. Visit again!")
+				sys.exit()
+
+		else:
+			print("Wrong choice. Please enter one of the below options")
+			print("1 2 3 4")
+			sys.exit()
+
+ram = Bank(1000, "Ram")
+ram.process_customer_request()
